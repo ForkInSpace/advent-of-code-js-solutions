13c35f463a3c11ae3feacea3bf9ea78e7fcd4d2f
Zaza <ts.zaza@gmail.com>
2020-10-24 20:25:47 +0300

    unrefactored solution

--- index.js
+++ index.js
@@ -1,33 +1,63 @@
-const fs = require('fs');
+const fs = require("fs");
 
-(async function(){
-  const rawInputs = await fetchInputs();
-  const inputArray = inputToArray(rawInputs);
-  const fuels = calcFuelRequredByEachModule(inputArray);
-  const totalSum = sum(fuels);
-  console.log(totalSum);
-})()
+(async function () {
+  const data = await fetchInputs();
+
+  let result = loopAndModify(data);
+  console.log(result);
+})();
 
 function fetchInputs() {
   return new Promise((resolve, reject) => {
-    fs.readFile('./input', 'utf-8', function(err, data) {
+    fs.readFile("./input", "utf-8", function (err, data) {
       if (err) return reject(err);
 
       resolve(data);
-    })
-  })
+    });
+  });
 }
 
-function inputToArray(content) {
-    return content.split('\n');
+const getAllCodes = (data) => {
+  return data.split(",").map(Number);
+};
+
+const getCodeChunks = (data) => {
+  let chunks = [];
+  while (data.length > 0) {
+    chunks.push(data.splice(0, 4));
+  }
+  return chunks;
+};
+
+const loopAndModify = (data) => {
+  let allCodes = getAllCodes(data);
+  let chunks = getCodeChunks(getAllCodes(data));
+  let chunkLength = chunks.length;
+  let output = null;
+
+  for (let indx = 0; indx < chunkLength; indx++) {
+    let op = chunks[indx][0];
+    let posOne = chunks[indx][1];
+    let posTwo = chunks[indx][2];
+    let posThree = chunks[indx][3];
+
+    if (op == 1) {
+      output = sum(allCodes[posOne], allCodes[posTwo]);
+      allCodes.splice(posThree, 1, output);
+    } else if (op == 2) {
+      output = multiply(allCodes[posOne], allCodes[posTwo]);
+      allCodes.splice(posThree, 1, output);
+    } else if (op == 99) {
+      return allCodes[0];
+    }
+    chunks = getCodeChunks([...allCodes]);
+  }
+};
+
+function sum(x, y) {
+  return x + y;
 }
 
-function calcFuelRequredByEachModule(args) {
-  return args.map(moduleMass => {
-    return Math.floor(moduleMass/3) - 2;
-  })
-}
-
-function sum(moduleFuels) {
-  return moduleFuels.reduce((total, fuel) => total + fuel);
+function multiply(x, y) {
+  return x * y;
 }
