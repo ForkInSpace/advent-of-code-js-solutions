364d7304b7b5fded2ac428807b465ac332d84cc3
Zaza <ts.zaza@gmail.com>
2020-12-05 21:24:22 +0200

    slap yet another nested loop (I'm sure there's a better way of doing this)

--- index.js
+++ index.js
@@ -25,8 +25,10 @@ function compareNumbersInArr(args) {
   const sorted = sortArr(args);
   for (let i = 0; i < sorted.length; i++) {
     for (let j = sorted.length - 1; j >= 0; j--) {
-      if (sum(sorted[i], sorted[j]) == 2020) {
-        return multiply(sorted[i], sorted[j]);
+      for (let k = 0; k < sorted.length; k++) {
+        if (sum(sorted[i], sorted[j], sorted[k]) == 2020) {
+          return multiply(sorted[i], sorted[j], sorted[k]);
+        }
       }
     }
   }
@@ -36,10 +38,10 @@ function sortArr(arr) {
   return arr.sort();
 }
 
-function sum(val1, val2) {
-  return val1 + val2;
+function sum(val1, val2, val3) {
+  return val1 + val2 + val3;
 }
 
-function multiply(val1, val2) {
-  return val1 * val2;
+function multiply(val1, val2, val3) {
+  return val1 * val2 * val3;
 }
