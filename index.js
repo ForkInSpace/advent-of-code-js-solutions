4de5040f46d199b3b7294152831d858423dc7fff
Zaza <ts.zaza@gmail.com>
2020-12-10 01:51:19 +0200

    solve day 4

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const fs = require('fs');
 
-(async function(){
+const requiredFields = [
+  'byr',
+  'iyr',
+  'eyr',
+  'hgt',
+  'hcl',
+  'ecl',
+  'pid'
+];
+
+(async () => {
   const rawInputs = await fetchInputs();
-  const inputArray = inputToArray(rawInputs);
-  const res = multipleSlopes(inputArray);
-  console.log(res);
+  const arr = toArray(rawInputs);
+  const passports = parseArr(arr);
+  const result = validatePassports(passports, requiredFields);
+  console.log(result);
 })()
 
 function fetchInputs() {
   return new Promise((resolve, reject) => {
-    fs.readFile('./input', 'utf-8', function(err, data) {
+    fs.readFile('./input', 'utf-8', (err, data) => {
       if (err) return reject(err);
 
       resolve(data);
@@ -17,44 +28,43 @@ function fetchInputs() {
   })
 }
 
-function inputToArray(content) {
-  const arr = content.split('\n');
-  result = arr.map(str => str.split(''));
-  return result;
+function toArray(content) {
+  const arr = content.split(/\n\s*\n/);
+  return arr.map(a => a.replace(/\n/g, ' ').split(' '))
 }
 
-function singleSlope(arr, xUnits, yUnits) {
-
-  let numberOfTrees = 0;
-  let x = 0;
-  let y = 0;
-  
-  while (y < arr.length) {
-    const adjustedX = x % arr[0].length;
-    const coordinate = arr[y][adjustedX];
-
-    if (coordinate === '#') { 
-      numberOfTrees++;
-    }
-
-    x += xUnits;
-    y += yUnits;
-  }
-
-  return numberOfTrees;
+function parseArr(arr) {
+  var mapped = {};
+  const res = arr
+    .map(field => {
+      let initialValue = {};
+      return field.reduce((mapped, i) => {
+        return {
+          ...mapped,
+          [i.split(':')[0]]: i.split(':')[1]
+        }
+      }, initialValue)
+    });
+  return res;
 }
 
-function multipleSlopes(arr) {
-  let xUnits = [1, 3 ,5 ,7, 1];
-  let yUnits = [1, 1, 1, 1, 2];
-
-  let numberOfTrees = new Array(5).fill(0);
-
-  for (let i = 0; i < xUnits.length; i++) {
-    let totalTrees = singleSlope(arr, xUnits[i], yUnits[i]);
-
-    numberOfTrees[i] = totalTrees;
+function validatePassport(passport, requiredFields) {
+  // refactor the mess
+  if (Object.keys(passport).length == 8) {
+    return true;
+  } else if (Object.keys(passport).length == 7) {
+    return Object
+    .keys(passport)
+    .every(e => {
+      return requiredFields.includes(e)
+    });
+  } else {
+    return false;
   }
-  
-  return numberOfTrees.reduce((a, b) => a * b);
+}
+
+function validatePassports(passports, requiredFields) {
+  return passports
+    .filter(p => validatePassport(p, requiredFields))
+    .length;
 }
